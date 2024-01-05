6a4f9314b22d78843cfeb0e31eeeb118a974d4b7
Diogo Sambento <diogoamorim135@outlook.com>
2024-01-05 17:24:30 +0000

    declarations file temporarily added to fix visual problem

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,30 @@
 import React from 'react'
 //N preciso de definir tipo do da função (do return) pq typescript sabe por causa do jsx
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
+import surfStrechImage from '../../resources/images/surfStrech.jpg';
 import surfImage from '../../resources/images/surf.jpg';
+import velaImage from '../../resources/images/vela.jpg';
+import velaSmileImage from '../../resources/images/velaSmile.jpg';
 
 export function Dashboard() {
     return (
-        <img src={surfImage} alt="surf"/>
-        // <Carousel>
-        //     <div>
-        //         <img src='../../resources/images/surfStrech.jpg' alt="surfStrech"/>
-        //         {/* <p className="legend">Legend 1</p> */}
-        //     </div>
-        //     <div>
-        //         <img src='./surf.jpg' alt="surf"/>
-        //         {/* <p className="legend">Legend 2</p> */}
-        //     </div>
-        //     <div>
-        //         <img src='../../resources/images/velaSmile.jpg' alt="velaSmile"/>
-        //         {/* <p className="legend">Legend 3</p> */}
-        //     </div>
-        //     <div>
-        //         <img src='../../resources/images/vela.jpg' alt="vela"/>
-        //         {/* <p className="legend">Legend 3</p> */}
-        //     </div>
-        // </Carousel>
+        <Carousel>
+            <div>
+                <img src={surfStrechImage} alt="surfStrech"/>
+            </div>
+            <div>
+                <img src={surfImage} alt="surf"/>
+            </div>
+            <div>
+                <img src={velaImage} alt="velaSmile"/>
+            </div>
+            <div>
+                <img src={velaSmileImage} alt="vela"/>
+            </div>
+        </Carousel>
     )
 }
 
