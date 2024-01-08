e757bef6c7fd0e735fec8c16d3f235d5b14ca3f2
Diogo Sambento <diogoamorim135@outlook.com>
2024-01-08 18:44:14 +0000

    First endpoint call: Sending hard coded post when clicking  'Add' button.

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,17 +1,49 @@
 import * as React from 'react';
 import { GridColDef} from '@mui/x-data-grid';
 import { GridToolbar } from '@mui/x-data-grid';
-
+import Button from '@mui/material/Button';
 import {StyledDataGridContainer, StyledDataGrid} from './styles.ts'
+import axios from 'axios';
 
 export const Table: React.FC<{rows: any[], columns: GridColDef[]}> = ({rows, columns}) => {
+
+
+    const handleAddClick = async () => {
+      const customerData = {
+        firstName: "Diogo",
+        lastName: "Sambento",
+        birthdate: "2000-12-07",
+        fiscalNumber: "237495759",
+        mobileNumber: "925977534"
+      };
+    
+      try {
+        const response = await axios.post('http://localhost:8080/customer', customerData);
+        console.log(response.data); // Handle the response as needed
+      } catch (error) {
+        console.error("Error adding customer:", error);
+      }
+    };
+    const CustomToolbar = () => (
+      <>
+        <GridToolbar />
+        <Button 
+          variant="contained" 
+          style={{ position: 'absolute', top: 10, right: 10 }}
+          onClick={() => {handleAddClick()}}
+        >
+          Add
+        </Button>
+      </>
+    );
+
     return (
       <StyledDataGridContainer>
         <StyledDataGrid
           rows={rows}
           columns={columns}
           slots={{
-            toolbar: GridToolbar,
+            toolbar: CustomToolbar,
           }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
